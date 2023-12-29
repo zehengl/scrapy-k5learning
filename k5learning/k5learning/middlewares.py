ed23fd0947de89119019d85665ded12c92fa9c2a
Zeheng Li <imzehengl@gmail.com>
2023-12-29 02:15:04 -0700

    clean up

--- k5learning/k5learning/middlewares.py
+++ k5learning/k5learning/middlewares.py
@@ -3,10 +3,9 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-from scrapy import signals
-
 # useful for handling different item types with a single interface
-from itemadapter import is_item, ItemAdapter
+from itemadapter import ItemAdapter, is_item
+from scrapy import signals
 
 
 class K5LearningSpiderMiddleware:
@@ -53,7 +52,7 @@ class K5LearningSpiderMiddleware:
             yield r
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info("Spider opened: %s" % spider.name)
 
 
 class K5LearningDownloaderMiddleware:
@@ -100,4 +99,4 @@ class K5LearningDownloaderMiddleware:
         pass
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info("Spider opened: %s" % spider.name)
